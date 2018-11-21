604c36c1bcb93d6ebb499a074f39e85afbc1de15
MoonDropX <minalinskyx@hotmail.com>
2018-11-22 01:06:33 +0800

    feat(Home): add poster viewer

---
+++ src/Components/Poster/index.tsx
@@ -0,0 +1,85 @@
+import * as React from 'react';
+import { Radio } from 'antd';
+import classNames from 'classnames';
+import styles from './index.module.less';
+import { STATIC_URL } from '../../consts';
+
+interface PosterProps {
+  posters: string[];
+}
+
+interface PosterStates {
+  selectedPoster: string;
+  selectedYear: string;
+}
+
+class Poster extends React.Component<PosterProps, PosterStates> {
+  public state = {
+    selectedPoster: '',
+    selectedYear: '',
+  };
+
+  private years: string[] = [];
+
+  public componentDidMount() {
+    const { posters } = this.props;
+    this.setState({
+      selectedPoster: posters[posters.length - 1],
+      selectedYear: posters[posters.length - 1].slice(0, 4),
+    });
+    posters.forEach(poster => {
+      const year = poster.slice(0, 4);
+      if (!this.years.find(y => y === year)) {
+        this.years.push(year);
+      }
+    });
+  }
+
+  private handleYearChange = (e: any) => {
+    this.setState({ selectedYear: e.target.value });
+  };
+
+  private handlePosterChange = (poster: string) => {
+    this.setState({ selectedPoster: poster });
+  };
+
+  public render() {
+    return (
+      <div className={styles.container}>
+        <Radio.Group
+          onChange={this.handleYearChange}
+          value={this.state.selectedYear}
+        >
+          {this.years.map(year => (
+            <Radio.Button value={year} key={year}>
+              {year}
+            </Radio.Button>
+          ))}
+        </Radio.Group>
+        <div className={styles.posterNameContainer}>
+          {this.props.posters
+            .filter(poster => poster.slice(0, 4) === this.state.selectedYear)
+            .map(poster => (
+              <span className={styles.posterBreadcrumb} key={poster}>
+                <span
+                  className={classNames(styles.posterBreadcrumbText, {
+                    [styles.active]: this.state.selectedPoster === poster,
+                  })}
+                  onClick={() => this.handlePosterChange(poster)}
+                >
+                  {poster.slice(4)}
+                </span>
+                <span className={styles.posterBreadcrumbDivider}>/</span>
+              </span>
+            ))}
+        </div>
+        <img
+          className={styles.poster}
+          src={`${STATIC_URL}/poster/${this.state.selectedPoster}.jpg`}
+        />
+      </div>
+    );
+  }
+}
+
+export default Poster;
--- src/Components/Routes/Home/index.tsx
+++ src/Components/Routes/Home/index.tsx
@@ -5,10 +5,10 @@ import { Query } from 'react-apollo';
 import moment from 'moment';
 import Card from '../../Card';
 import Pill from '../../Pill';
+import Poster from '../../Poster';
 import logo from '../../../logo.png';
 import payImage from './pay.png';
 import styles from './index.module.less';
-import { STATIC_URL } from '../../../consts';
 
 const { Content } = Layout;
 
@@ -49,37 +49,31 @@ export default class Home extends React.Component {
           }
         `}
       >
-        {({ loading, error, data }) => (
-          <Content className={styles.content}>
-            <div className={styles.logoblock}>
-              <img src={logo} className={styles.topLogo} />
-              <h1>Naberius</h1>
-            </div>
-            <Card loading={loading}>
-              {!loading && (
-                <div>
-                  <Badge status={status[data.serverStatus]} text="状态" />
-                  <Row>{this.filesToPills(data.uploadFiles)}</Row>
-                </div>
-              )}
-            </Card>
-            <div className={styles.beg}>
-              <p>
-                <del>生活所迫只能要饭了</del>, 支付宝
-              </p>
-              <img src={payImage} alt="支付宝@13021225563" />
-            </div>
-            {!loading && (
-              <div className={styles.postContainer}>
-                <img
-                  src={`${STATIC_URL}/poster/${
-                    data.posters[data.posters.length - 1]
-                  }.jpg`}
-                />
+        {({ loading, error, data }) => {
+          return (
+            <Content className={styles.content}>
+              <div className={styles.logoblock}>
+                <img src={logo} className={styles.topLogo} />
+                <h1>Naberius</h1>
               </div>
-            )}
-          </Content>
-        )}
+              <Card loading={loading}>
+                {!loading && (
+                  <div>
+                    <Badge status={status[data.serverStatus]} text="状态" />
+                    <Row>{this.filesToPills(data.uploadFiles)}</Row>
+                  </div>
+                )}
+              </Card>
+              <div className={styles.beg}>
+                <p>
+                  <del>生活所迫只能要饭了</del>, 支付宝
+                </p>
+                <img src={payImage} alt="支付宝@13021225563" />
+              </div>
+              {!loading && <Poster posters={data.posters} />}
+            </Content>
+          );
+        }}
       </Query>
     );
   }
