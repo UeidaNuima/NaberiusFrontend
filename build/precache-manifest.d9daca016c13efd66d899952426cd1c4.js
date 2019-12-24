17ac7043d584bf9e0b77caf41e48ec2a9b6820ad
MoonDropX <minalinskyx@hotmail.com>
2019-12-24 16:31:17 +0800

    build: new

---
+++ build/precache-manifest.d9daca016c13efd66d899952426cd1c4.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9764e9db96238d21353f606ae27b1ac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "02236713248a5cb87736",
+    "url": "/static/css/2.c3d7a629.chunk.css"
+  },
+  {
+    "revision": "e342fc52dd2e550539ae",
+    "url": "/static/css/main.bf93ca67.chunk.css"
+  },
+  {
+    "revision": "02236713248a5cb87736",
+    "url": "/static/js/2.1d2e4b80.chunk.js"
+  },
+  {
+    "revision": "d6a7472a13919fb2e347fd9298bef2a8",
+    "url": "/static/js/2.1d2e4b80.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e342fc52dd2e550539ae",
+    "url": "/static/js/main.aaabd1da.chunk.js"
+  },
+  {
+    "revision": "8bd58515cb0081862933",
+    "url": "/static/js/runtime-main.c42d8ef7.js"
+  },
+  {
+    "revision": "509ca4a9c004f5d5c1500967c77459af",
+    "url": "/static/media/logo.509ca4a9.png"
+  },
+  {
+    "revision": "f5dab129a72fd3bbfca91088cd6b0a95",
+    "url": "/static/media/pay.f5dab129.png"
+  }
+]);
