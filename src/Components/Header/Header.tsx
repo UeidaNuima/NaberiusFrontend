fd1f00f71ba97baab5c16d7558ddfacacaad26d0
MoonDropX <minalinskyx@hotmail.com>
2019-12-24 18:59:41 +0800

    refactor(header): class to hooks

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,69 +1,53 @@
-import * as React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { Layout, Menu } from 'antd';
-import { withRouter, RouteComponentProps } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 import logo from '../../logo.png';
 import styles from './Header.module.less';
 
 const { Header } = Layout;
 const { SubMenu, Item } = Menu;
 
-interface HeaderBarStates {
-  active: string;
-}
+const HeaderBar: React.FC = () => {
+  const [active, setActive] = useState('/');
+  const location = useLocation();
+  useEffect(() => {
+    setActive(location.pathname.split('/')[1]);
+  }, [location]);
 
-export default withRouter(
-  class HeaderBar extends React.Component<
-    RouteComponentProps<any>,
-    HeaderBarStates
-  > {
-    public state = {
-      active: '/',
-    };
-    public componentDidMount() {
-      this.setState({
-        active: this.props.location.pathname.split('/')[1],
-      });
-    }
-    public componentWillReceiveProps(nextProps: any) {
-      this.setState({
-        active: nextProps.location.pathname.split('/')[1],
-      });
-    }
-    public render() {
-      return (
-        <Header className={styles.header}>
-          <Link to="/">
-            <img alt="logo" src={logo} className={styles.logo} />
-          </Link>
-          <Menu
-            mode="horizontal"
-            className={styles.headerMenu}
-            selectedKeys={[this.state.active]}
-          >
-            <Item key="unit">
-              <Link to="/unit">单位</Link>
-            </Item>
-            <Item key="quest">
-              <Link to="/quest">关卡</Link>
-            </Item>
-            <SubMenu title="...">
-              <Item key="class">
-                <Link to="/class">职业</Link>
-              </Item>
-              <Item key="skill">
-                <Link to="/skill">技能</Link>
-              </Item>
-              <Item key="ability">
-                <Link to="/ability">被动</Link>
-              </Item>
-            </SubMenu>
-            {/* <Item key="emoji">
+  return (
+    <Header className={styles.header}>
+      <Link to="/">
+        <img alt="logo" src={logo} className={styles.logo} />
+      </Link>
+      <Menu
+        mode="horizontal"
+        className={styles.headerMenu}
+        selectedKeys={[active]}
+      >
+        <Item key="unit">
+          <Link to="/unit">单位</Link>
+        </Item>
+        <Item key="quest">
+          <Link to="/quest">关卡</Link>
+        </Item>
+        <SubMenu title="...">
+          <Item key="class">
+            <Link to="/class">职业</Link>
+          </Item>
+          <Item key="skill">
+            <Link to="/skill">技能</Link>
+          </Item>
+          <Item key="ability">
+            <Link to="/ability">被动</Link>
+          </Item>
+        </SubMenu>
+        {/* <Item key="emoji">
               <Link to="/emoji">机器狗</Link>
             </Item> */}
-          </Menu>
-        </Header>
-      );
-    }
-  },
-);
+      </Menu>
+    </Header>
+  );
+};
+
+export default HeaderBar;
