a625f1031b2769a29338f330d221bc60787fc60f
MoonDropX <minalinskyx@hotmail.com>
2020-01-03 22:47:05 +0800

    dump

---
+++ docs/precache-manifest.3c793645b016718c58a1c3f091022f92.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9b1f0d0e3fb1b5f9f7dd5cba8e80718",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1dd6b540530c810f9249",
+    "url": "/static/css/2.c3d7a629.chunk.css"
+  },
+  {
+    "revision": "2283988b2474f3781541",
+    "url": "/static/css/main.7a33811c.chunk.css"
+  },
+  {
+    "revision": "1dd6b540530c810f9249",
+    "url": "/static/js/2.3e31672e.chunk.js"
+  },
+  {
+    "revision": "d6a7472a13919fb2e347fd9298bef2a8",
+    "url": "/static/js/2.3e31672e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2283988b2474f3781541",
+    "url": "/static/js/main.031d64ab.chunk.js"
+  },
+  {
+    "revision": "8bd58515cb0081862933",
+    "url": "/static/js/runtime-main.c42d8ef7.js"
+  },
+  {
+    "revision": "509ca4a9c004f5d5c1500967c77459af",
+    "url": "/static/media/logo.509ca4a9.png"
+  },
+  {
+    "revision": "f5dab129a72fd3bbfca91088cd6b0a95",
+    "url": "/static/media/pay.f5dab129.png"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7f037e8bf13ad12e1a81446b5183cb16.js"
+  "/precache-manifest.3c793645b016718c58a1c3f091022f92.js"
 );
 
 self.addEventListener('message', (event) => {
